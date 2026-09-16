9191b2f79a8413fed1ed23123dea2df8a9b4570d
borgut <borguti@gmail.com>
2026-09-16 23:16:01 +0200

    Add server.js

---
+++ server.js
@@ -0,0 +1,4 @@
+const express=require('express'); const path=require('path'); const app=express();
+app.use(express.static(path.join(__dirname,'public'),{maxAge:'1d'}));
+app.get('*',(_,res)=>res.sendFile(path.join(__dirname,'public','index.html')));
+app.listen(process.env.PORT||3000);
